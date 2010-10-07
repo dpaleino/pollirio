5203a7b9d19069b9a6678cbacdc7f50d8bbfdac7
David Paleino <dapal@debian.org>
2010-10-07 17:59:48 +0200

    Fix it again

--- pollirio/modules/misc.py
+++ pollirio/modules/misc.py
@@ -1,5 +1,6 @@
 from pollirio.modules import expose
+from pollirio import choose_dest
 
 @expose('version')
 def version(bot, ievent):
-    bot.msg(ievent.channel, '%s: this is Pollirio, version 0.1' % ievent.nick)
+    bot.msg(choose_dest(ievent), '%s: this is Pollirio, version 0.1' % ievent.nick)
