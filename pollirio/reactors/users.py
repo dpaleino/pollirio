14e8e3944b732bb62c53e22c54a5eac7ffa5b9a4
David Paleino <dapal@debian.org>
2013-03-08 20:44:04 +0100

    Add reactor for Turid

--- pollirio/reactors/users.py
+++ pollirio/reactors/users.py
@@ -3,6 +3,8 @@
 from pollirio.reactors import expose
 from pollirio import choose_dest
 
+import random
+
 @expose('mappina')
 def mappina(bot, ievent):
 	bot.msg(choose_dest(ievent), 'aaaaah, la STREGA!')
@@ -10,3 +12,17 @@ def mappina(bot, ievent):
 @expose('linxys')
 def linxys(bot, ievent):
 	bot.msg(choose_dest(ievent), 'Linxys :***')
+
+@expose(r'\bturid\b')
+def turid(bot, ievent):
+    sentences = [
+        'che ha combinato Turid stavolta?!',
+        'ma Turid ha bevuto?',
+        'in alto i calici per Turid!',
+        'evviva Turid!!!',
+        'fate reclutare gente a Turid!',
+        'Turid mi fa i dispetti :(',
+        'Turid: ILLIMITATAMENTEEEEEEE',
+    ]
+    msg = random.choice(sentences)
+	bot.msg(choose_dest(ievent), msg)
