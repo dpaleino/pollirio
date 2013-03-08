1c4227ad11850b9bbe90dafe448254f55e9a97f7
David Paleino <dapal@debian.org>
2013-03-08 19:10:09 +0100

    Run the regex case-insensitively

--- pollirio/reactors/__init__.py
+++ pollirio/reactors/__init__.py
@@ -13,7 +13,7 @@ def expose(text, args=None):
 
 def reactor_run(line, *args):
     for k in reactors.keys():
-        if re.findall(k, line):
+        if re.findall(k, line, re.IGNORECASE):
             return reactors.get(k)["func"](*args)
 
 from users import *
