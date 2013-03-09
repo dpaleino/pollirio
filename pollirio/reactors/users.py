04017bcabbd7703022cf04aef25b7d9e6b5f97c4
David Paleino <dapal@debian.org>
2013-03-09 20:27:23 +0100

    Add more sentences, and make them look more random

--- pollirio/reactors/users.py
+++ pollirio/reactors/users.py
@@ -5,13 +5,29 @@ from pollirio import choose_dest
 
 import random
 
+def randomize_msg(sentences):
+	times = random.randint(0, len(sentences) / 3)
+	while times > 0:
+		index = random.randint(0, len(sentences) - 1)
+		sentences.insert(index, None)
+		times -= 1
+	return random.choice(sentences)
+
 @expose('mappina')
 def mappina(bot, ievent):
 	bot.msg(choose_dest(ievent), 'aaaaah, la STREGA!')
 
 @expose('linxys')
 def linxys(bot, ievent):
-	bot.msg(choose_dest(ievent), 'Linxys :***')
+	sentences = [
+		'Linxys :***',
+		'Linxys ti difendo io!',
+		'hey, Linxys! Non tradirmi! :(',
+		'come recluta Linxys...',
+	]
+	msg = randomize_msg(sentences)
+	if msg:
+		bot.msg(choose_dest(ievent), msg)
 
 @expose(r'\bturid\b')
 def turid(bot, ievent):
@@ -24,5 +40,17 @@ def turid(bot, ievent):
 		'Turid mi fa i dispetti :(',
 		'Turid: ILLIMITATAMENTEEEEEEE',
 	]
-	msg = random.choice(sentences)
-	bot.msg(choose_dest(ievent), msg)
+	msg = randomize_msg(sentences)
+	if msg:
+		bot.msg(choose_dest(ievent), msg)
+
+@expose('x3rr3x')
+@expose(r'\berre\b')
+def erre(bot, ievent):
+	sentences = [
+		'vola, x3rr3x, vola!',
+		'x3rr3x sei carico stasera?',
+	]
+	msg = randomize_msg(sentences)
+	if msg:
+		bot.msg(choose_dest(ievent), msg)
