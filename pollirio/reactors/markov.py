63f0c0e80d3a2583f0387f80c608a173d75adc92
David Paleino <dapal@debian.org>
2013-04-09 22:25:10 +0200

    Differentiate learning and talk between #fanculo and the world

--- pollirio/reactors/markov.py
+++ pollirio/reactors/markov.py
@@ -11,8 +11,10 @@ try:
     markov_chains = cPickle.load(open(markov_data))
 except IOError:
     markov_chains = {}
+    markov_chains['all'] = {}
+    markov_chains['fanculo'] = {}
 
-def update_chains(lines):
+def update_chains(lines, alternative=False):
     has_prev = False
     for line in lines:
         for cur_word in line.split():
@@ -22,13 +24,21 @@ def update_chains(lines):
                     prev_word = cur_word
                     has_prev = True
                 else:
-                    markov_chains.setdefault(prev_word, []).append(cur_word)
+                    if alternative:
+                        markov_chains['fanculo'].setdefault(prev_word, []).append(cur_word)
+                    else:
+                        markov_chains['all'].setdefault(prev_word, []).append(cur_word)
                     prev_word = cur_word
 
-def make_sentence(words=None):
+def make_sentence(words=None, alternative=False):
     while True:
         if not words:
-            word = random.choice(markov_chains.keys())
+            keys = markov_chains['all'].keys()
+            if alternative:
+                additional_keys = [x for x in markov_chains['fanculo'].keys() if x not in keys]
+                word = random.choice(keys + additional_keys)
+            else:
+                word = random.choice(keys)
         else:
             word = random.choice(words)
         if word[-1] not in ('.','?'):
@@ -36,7 +46,12 @@ def make_sentence(words=None):
     generated_sentence = word.capitalize()
     while word[-1] not in ('.','?'):
         try:
-            newword = random.choice(markov_chains[word])
+            subchain = markov_chains['all'][word]
+            if alternative:
+                additional_subchain = [x for x in markov_chains['fanculo'][word] if x not in subchain]
+                newword = random.choice(subchain + additional_subchain)
+            else:
+                newword = random.choice(subchain)
             generated_sentence += ' '+newword
             word = newword #TODO fix possible crash if this is not a key (last word parsed)
         except KeyError:
@@ -47,17 +62,26 @@ def make_sentence(words=None):
 @expose('.*')
 def learn(bot, ievent):
     if ievent.msg.startswith(conf.nickname) or (ievent.channel == '#fanculo' and conf.nickname in ievent.msg):
-        # pass control to talk()
-        talk(bot, ievent)
+        if ievent.channel == '#fanculo':
+            # learn from here too.
+            update_chains([ievent.msg.replace(conf.nickname, '')], True)
+            words = ievent.msg.replace(conf.nickname, '').split()
+            talk(bot, ievent, words, True)
+        else:
+            talk(bot, ievent)
         return
-    if ievent.channel.startswith('#') and ievent.channel != '#fanculo':
-        update_chains([ievent.msg])
+
+    if ievent.channel.startswith('#'):
+        if ievent.channel == '#fanculo':
+            update_chains([ievent.msg], True)
+        else:
+            update_chains([ievent.msg])
         #print markov_chains
         cPickle.dump(markov_chains, open(markov_data, 'w'))
 
 # commented because triggered from learn()
 #@expose('^%s' % conf.nickname)
-def talk(bot, ievent):
+def talk(bot, ievent, words=None, alternative=False):
     #source = 'data/pg28867.txt'
     #mc = create_chains(open(source))
-    bot.msg(choose_dest(ievent), '%s: %s' % (ievent.nick, make_sentence()))
+    bot.msg(choose_dest(ievent), '%s: %s' % (ievent.nick, make_sentence(words, alternative)))
