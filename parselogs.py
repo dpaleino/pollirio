a83f51733850b53e1bd24ace88dcc3e8b5c1151f
David Paleino <dapal@debian.org>
2014-01-13 01:11:44 +0100

    New log format, and log parser

---
+++ parselogs.py
@@ -0,0 +1,151 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import time
+from collections import defaultdict
+from pprint import pprint
+
+channels = ['ufficiali', 'accademia-ei', 'reclute-war', 'erep-war']
+
+ufficiali = {
+    'Aelagon': 'Aelagon',
+    'Aelagon|AwAy': 'Aelagon',
+    'Aelagon|BNC': 'Aelagon',
+    '[AFK]_KronosLostSoul_[AFK]': 'KronosLostSoul',
+    'Aleisteir': 'Aleisteir',
+    'armageddone': 'armageddone',
+    'bic': 'bic',
+    'bicAFK': 'bic',
+    'BNeCtore': 'BNeCtore',
+    'Cabla|afk': 'Cablatore',
+    'Cabla|cena': 'Cablatore',
+    'Cablatore': 'Cablatore',
+    'Cablatore|gnam': 'Cablatore',
+    'Cablottolo': 'Cablatore',
+    'CMagro': 'CMagro',
+    'CMagro|afk': 'CMagro',
+    'CMagro|BNC': 'CMagro',
+    'corvobianco': 'corvobianco',
+    'daniele81': 'daniele81',
+    'daniele81-auei': 'daniele81',
+    'daniele81-pappa': 'daniele81',
+    'dapal': 'dapal',
+    'dapal|afk': 'dapal',
+    'dapal|cena': 'dapal',
+    'Dark_afk': 'Dark_Thunder',
+    'Dark_Busy': 'Dark_Thunder',
+    'DEVILPIERO': 'DEVILPIERO',
+    'dirk|afk': 'dirkfelpy89',
+    'dirk|coffee': 'dirkfelpy89',
+    'dirkfelpy89': 'dirkfelpy89',
+    'Fragiopa': 'Fragiopa',
+    'Fragiopa-AFK': 'Fragiopa',
+    'Gregory_IV': 'Gregory_IV',
+    'Gregory|offline': 'Gregory_IV',
+    'Gregory|x_x': 'Gregory_IV',
+    'Halex19': 'Halex19',
+    'Halex|BNC': 'Halex19',
+    'Halex|BUSY': 'Halex19',
+    'IlMessaggero': 'MercuriusTheOne',
+    'investigatore_pollo': 'armageddone',
+    'kimi89na': 'kimi89na',
+    'kimi89na|Afk': 'kimi89na',
+    'kimi89na|BNC': 'kimi89na',
+    'kimi89na|busy': 'kimi89na',
+    'kimi89na|study': 'kimi89na',
+    'Kimi|euei': 'Kimilla',
+    'Kimilla': 'Kimilla',
+    'Kimillodora': 'Kimilla',
+    'Kimmodora': 'Kimilla',
+    'KronosLostSoul': 'KronosLostSoul',
+    'L4mer': 'Gregory_IV',
+    'Linxys': 'Linxys',
+    'Linxys|hawaii': 'Linxys',
+    'MercuriusTheOne': 'MercuriusTheOne',
+    'nabbic': 'bic',
+    'nikidellicolli': 'nikidellicolli',
+    'taurus85': 'taurus85',
+    'yap13': 'yap13',
+}
+
+stats = defaultdict(dict)
+
+def reversed_lines(file):
+    "Generate the lines of file in reverse order."
+    part = ''
+    for block in reversed_blocks(file):
+        for c in reversed(block):
+            if c == '\n' and part:
+                yield part[::-1]
+                part = ''
+            part += c
+    if part: yield part[::-1]
+
+def reversed_blocks(file, blocksize=4096):
+    "Generate blocks of file's contents in reverse order."
+    file.seek(0, os.SEEK_END)
+    here = file.tell()
+    while 0 < here:
+        delta = min(blocksize, here)
+        here -= delta
+        file.seek(here, os.SEEK_SET)
+        yield file.read(delta)
+
+for ch in channels:
+    for line in open('logs/%s.log' % ch, 'r').readlines():
+        m = re.match('^\[([^\] ]+)[^\]]+\] <([^>]+)> .*', line)
+        if m:
+            date = m.group(1)
+            nick = m.group(2)
+
+            try:
+                stats[ufficiali[nick]][ch] = date
+            except KeyError as ex:
+                pass
+
+# Sanitize stats
+for officer in stats:
+    for ch in channels:
+        if not stats[officer].has_key(ch):
+            stats[officer][ch] = None
+
+#pprint(dict(stats))
+
+print """<html>
+<head>
+<title>Controllo frequenza Ufficiali</title>
+</head>
+<body>
+<table border='1'>
+<thead>
+<tr>
+<th>Ufficiale</th>
+<th>#Accademia-EI</th>
+<th>#erep-war</th>
+<th>#reclute-war</th>
+<th>#ufficiali</th>
+</tr>
+</thead>
+<tbody>"""
+
+for officer in sorted(stats.keys(), key=lambda x: x.lower()):
+    print """<tr>
+<td>%s</td>
+<td>%s</td>
+<td>%s</td>
+<td>%s</td>
+<td>%s</td>
+</tr>""" % (
+        officer,
+        stats[officer]['accademia-ei'],
+        stats[officer]['erep-war'],
+        stats[officer]['reclute-war'],
+        stats[officer]['ufficiali']
+    )
+
+print """</tbody>
+</table>
+</body>
+</html>"""
--- pollirio/main.py
+++ pollirio/main.py
@@ -25,7 +25,7 @@ class Logger:
 
     def log(self, message):
         """Write a message to the file."""
-        timestamp = time.strftime("[%H:%M:%S]", time.localtime(time.time()))
+        timestamp = time.strftime("[%d/%m/%Y %H:%M:%S]", time.localtime(time.time()))
         self.file.write('%s %s\n' % (timestamp, message))
         self.file.flush()
 
